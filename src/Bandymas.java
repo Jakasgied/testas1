e274f67fd34fd548f40231d95c76160f0e6fc5b0
Giedrius J <giedrius.jakulevicius@gmail.com>
2021-01-04 17:41:55 +0200

    Initial commit

---
+++ src/Bandymas.java
@@ -0,0 +1,14 @@
+public class Bandymas {
+    public static void  main(String []args) {
+        String x;
+        x = "labas";
+
+        int y;
+        y=3;
+        int z;
+        z=2;
+        int sum;
+        sum= y + z;
+        System.out.println(sum);
+    }
+}
